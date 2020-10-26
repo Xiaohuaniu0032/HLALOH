da428ff436eb6cfc9ea64058425348e262933678
fulongfei <lffu_0032@outlook.com>
2020-10-26 10:38:21 +0800

    fix ASCAT no solution problem

--- tools/ascat.r
+++ tools/ascat.r
@@ -19,11 +19,26 @@ names(purity) <- NULL
 ploidy <- ascat.output$ploidy
 names(ploidy) <- NULL
 
+purity_new <- NULL
+ploidy_new <- NULL
+
+if (is.null(purity) | is.null(ploidy)) {
+	# no solution for purity & ploidy, return NA
+	# set purity as 100% and ploidy as 2 when we can not get solution for purity & ploidy
+	purity_new <- 1
+	ploidy_new <- 2
+	print("WARNING:This sample can not get optimal purity & ploidy by ASCAT software")
+}else{
+	purity_new <- purity
+	ploidy_new <- ploidy
+}
+
+val <- c(purity_new,ploidy_new)
+names(val) <- c("purity","ploidy")
+
 print(paste("purity is:",purity,sep=""))
 print(paste("ploidy is:",ploidy,sep=""))
 
-val <- c(purity,ploidy)
-names(val) <- c("purity","ploidy")
 write.table(val,file=of,quote=FALSE,row.names=TRUE,col.names=FALSE)
 
 
