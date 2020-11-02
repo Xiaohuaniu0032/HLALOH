5f364e63a31996cfc0fea36a176f001bcec2ba53
fulongfei <lffu_0032@outlook.com>
2020-11-02 12:59:09 +0800

    treat for case when nrow==0

--- tools/hla_baf.r
+++ tools/hla_baf.r
@@ -13,31 +13,47 @@ a <- read.table(hla_a_baf,header=TRUE,sep="\t")
 b <- read.table(hla_b_baf,header=TRUE,sep="\t")
 c <- read.table(hla_c_baf,header=TRUE,sep="\t")
 
+
 # plot hla a
-plot(NULL,xlim=c(0,max(a$pos1)),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_A BAF")
-for (i in 1:nrow(a)){
-	val <- a[i,]
-	points(val$pos1,val$baf1,pch=20)
-	points(val$pos1,val$baf2,pch=20)
+if (nrow(a) != 0){
+    # have enough het snp pos
+    plot(NULL,xlim=c(0,max(a$pos1)),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_A BAF")
+    for (i in 1:nrow(a)){
+        val <- a[i,]
+        points(val$pos1,val$baf1,pch=20)
+        points(val$pos1,val$baf2,pch=20)
+    }
+}else{
+    plot(NULL,xlim=c(0,1000),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_A BAF")
 }
 abline(h=0.5,col="blue")
 
+
 # plot hla b
-plot(NULL,xlim=c(0,max(b$pos1)),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_B BAF")
-for (i in 1:nrow(b)){
-	val <- b[i,]
-	points(val$pos1,val$baf1,pch=20)
-	points(val$pos1,val$baf2,pch=20)
+if (nrow(b) != 0){
+    plot(NULL,xlim=c(0,max(b$pos1)),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_B BAF")
+    for (i in 1:nrow(b)){
+        val <- b[i,]
+        points(val$pos1,val$baf1,pch=20)
+        points(val$pos1,val$baf2,pch=20)
+    }
+}else{
+    plot(NULL,xlim=c(0,1000),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_B BAF")
 }
 abline(h=0.5,col="blue")
 
 # plot hla c
-plot(NULL,xlim=c(0,max(c$pos1)),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_C BAF")
-for (i in 1:nrow(c)){
-	val <- c[i,]
-	points(val$pos1,val$baf1,pch=20)
-	points(val$pos1,val$baf2,pch=20)
+if (nrow(c) != 0){
+    plot(NULL,xlim=c(0,max(c$pos1)),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_C BAF")
+    for (i in 1:nrow(c)){
+        val <- c[i,]
+        points(val$pos1,val$baf1,pch=20)
+        points(val$pos1,val$baf2,pch=20)
+    }
+}else{
+    plot(NULL,xlim=c(0,1000),ylim=c(0,1),xlab="Pos Index",ylab="BAF",xaxt="n",xaxs="i",main="HLA_C BAF")
 }
 abline(h=0.5,col="blue")
+
 
 dev.off()
