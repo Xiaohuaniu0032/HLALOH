2eebdfa733144f6066817774c08fd2c82bf84def
fulongfei <lffu_0032@outlook.com>
2020-11-02 10:32:08 +0800

    update test2/run.sh

--- test2/run.sh
+++ test2/run.sh
@@ -1,4 +1,5 @@
 nbam='/home/fulongfei/workdir/hla_loh/program_test/downsample_bam/20091701N.sort.bam'
 tbam='/home/fulongfei/workdir/hla_loh/program_test/downsample_bam/20091701T.sort.bam'
 
-/home/fulongfei/miniconda3/bin/python3 ../predictHLALOH.v2.py -tbam $tbam -nbam $nbam -tname 20091701T -nname 20091701N -od $PWD -p 889
+#/home/fulongfei/miniconda3/bin/python3 ../predictHLALOH.v2.py -tbam $tbam -nbam $nbam -tname 20091701T -nname 20091701N -od $PWD -p 889
+/home/fulongfei/miniconda3/bin/python3 ../predictHLALOH.v2.py -tbam $tbam -nbam $nbam -tname 20091701T -nname 20091701N -od $PWD
