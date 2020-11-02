39f9a8c21e3405da95ed6aa1a46411bd30d654d0
fulongfei <lffu_0032@outlook.com>
2020-11-02 15:55:49 +0800

    add limit_qsub_Jobs.sh

--- tools/limit_qsub_Jobs.sh
+++ tools/limit_qsub_Jobs.sh
@@ -1,6 +1,22 @@
 runsh=$1
+#JobIDer=$2
+Num=15
+
 
 cat $runsh | while read line;
 do
-	# check runing jobs and waiting jobs
-		
+	while true;do
+		# check runing jobs and waiting jobs
+		#nJobs=$(qstat|grep $JobIDer|wc -l)
+		nJobs=$(qstat|wc -l)
+		#echo $num
+		if [ $nJobs -lt $Num ];then
+			d=$(dirname $line)
+			cd $d
+			qsub -l "mem=5g" -q all $line
+			break
+		else
+			sleep 120
+		fi
+	done
+done
