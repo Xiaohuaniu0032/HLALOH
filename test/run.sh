0dde566f6931afc1ae09fbfb19a7a15e8d8dfc20
fulongfei <lffu_0032@outlook.com>
2021-01-25 17:17:09 +0800

    remove old test result

--- test/run.sh
+++
@@ -1,8 +0,0 @@
-nbam='/home/fulongfei/workdir/hla_loh/program_test/downsample_bam/20091701N.sort.bam'
-tbam='/home/fulongfei/workdir/hla_loh/program_test/downsample_bam/20091701T.sort.bam'
-bamdir='/home/fulongfei/workdir/hla_loh/program_test/downsample_bam'
-bed='/home/wangce/workdir/database/humandb/panel/889genes_20191225.bed'
-gatkdir='/home/fulongfei/workdir/git_repo/HLALOH/gatkDir/picard-tools-1.119'
-ref='/home/fulongfei/workdir/git_repo/HLALOH/DB/cnv_ref/889/DB'
-
-/home/fulongfei/miniconda3/bin/python3 ../predictHLALOH.py -nbam $nbam -tbam $tbam -bed $bed -nname 20091701N -tname 20091701T -ref $ref -p 889 -od $PWD
