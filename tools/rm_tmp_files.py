24587c75fad08e7f1821866fb72f9b7e756689bb
fulongfei <lffu_0032@outlook.com>
2021-02-22 17:00:00 +0800

    update

--- tools/rm_tmp_files.py
+++ tools/rm_tmp_files.py
@@ -14,8 +14,9 @@ def rm_file(infile):
         print("not find %s file." % (infile))
 
 
+print("remove temp files...")
+
 for s in samlist:
-    print("remove temp files...")
     # rm extract_HLA_reads() tmp files
     hla_sam = "%s/%s.hla.sam" % (outdir,s)
     rm_file(hla_sam)
@@ -24,7 +25,7 @@ for s in samlist:
     rm_file(hla_bam)
 
     sort_bam = "%s/%s.sort_by_name.bam" % (outdir,s)
-    rm_file(hla_bam)
+    rm_file(sort_bam)
 
     # rm get_proper_aln_bam.py tmp files
     sam = "%s/%s.chr6region.patient.reference.hlas.sam" % (outdir,s)
